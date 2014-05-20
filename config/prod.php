da94c9e6c9289cf48d0dd2eeec07c363fc87f61b
Barnaby Walters <barnaby@waterpigs.co.uk>
2014-05-20 11:50:32 +0000

    Added call to create a log folder automatically

--- config/prod.php
+++ config/prod.php
@@ -5,6 +5,7 @@ use Silex\Provider\MonologServiceProvider;
 // Production config goes here.
 // $app['blahblahblah'] = 'blah';
 
+@mkdir(__DIR__.'/../logs/');
 $app->register(new MonologServiceProvider(), array(
 	'monolog.logfile' => __DIR__.'/../logs/silex.log',
 ));
